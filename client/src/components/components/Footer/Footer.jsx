691dcb5afe08f2009ee40511526c9e30978a5d4f
asmar-shikhamirli <asmar.shikhamirli.std@bhos.edu.az>
2022-04-25 18:13:43 +0400

    initial commit

---
+++ client/src/components/components/Footer/Footer.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+import FontAwesome from "react-fontawesome"
+
+// importting components
+
+
+// importing styles
+import {StyledFooter} from "./StyledFooter"
+
+const Footer = () => (
+    <StyledFooter>
+        <div className="footer-content">
+            <a href = "https://github.com/yashodeepchikte">
+                Made by Asmar and Artogrul
+            </a>
+        </div>
+    </StyledFooter>
+)
+
+export default Footer
